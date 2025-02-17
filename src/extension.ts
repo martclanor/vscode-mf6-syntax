ce1576f97421dd806f09ba11dc0e9d49e4c8d40c
martclanor <mart.dayniel.clanor@witteveenbos.com>
2025-02-17 21:28:02 +0100

    Show info message for mf6-ify command

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,12 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand("mf6-syntax.mf6-ify", () => {
     if (vscode.window.activeTextEditor) {
-      vscode.languages.setTextDocumentLanguage(
-        vscode.window.activeTextEditor.document,
-        "mf6",
+      const activeEditor = vscode.window.activeTextEditor;
+      const fileName = activeEditor.document.fileName.split("/").pop();
+
+      vscode.languages.setTextDocumentLanguage(activeEditor.document, "mf6");
+      vscode.window.showInformationMessage(
+        `MODFLOW 6 language mode set for: ${fileName}`,
       );
     }
   });
