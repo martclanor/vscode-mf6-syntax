f59ae594388bf239b29fd9ca239d1505bb2a8dcf
martclanor <mart.dayniel.clanor@witteveenbos.com>
2025-02-22 09:50:23 +0100

    Remove unnecessary info message
    Based on PR review comment: https://github.com/martclanor/vscode-mf6-syntax/pull/40#discussion_r1966470789

--- src/commands/mf6-ify.ts
+++ src/commands/mf6-ify.ts
@@ -3,11 +3,6 @@ import * as vscode from "vscode";
 export function mf6ify() {
   if (vscode.window.activeTextEditor) {
     const activeEditor = vscode.window.activeTextEditor;
-    const fileName = activeEditor.document.fileName.split("/").pop();
-
     vscode.languages.setTextDocumentLanguage(activeEditor.document, "mf6");
-    vscode.window.showInformationMessage(
-      `MODFLOW 6 language mode set for: ${fileName}`,
-    );
   }
 }
