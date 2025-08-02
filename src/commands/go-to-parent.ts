af539c9cba4c0f1f235f0d9a83d35dcaa2e7aaae
martclanor <mart.dayniel.clanor@witteveenbos.com>
2025-08-02 17:42:08 +0200

    Exclude some files in goToParent command (#128)
    * Restrict goToParent command to mf6 files only
    
    * Exclude mf6 output files from possible parent
    
    * Prefer guard clause
    
    * Exclude other binary and output files
    
    * Refactor to group excluded extensions

--- src/commands/go-to-parent.ts
+++ src/commands/go-to-parent.ts
@@ -3,7 +3,14 @@ import * as path from "path";
 
 export async function goToParent() {
   if (vscode.window.activeTextEditor) {
-    const fileUri = vscode.window.activeTextEditor.document.uri;
+    const document = vscode.window.activeTextEditor.document;
+    if (document.languageId !== "mf6") {
+      vscode.window.showInformationMessage(
+        "Go-to-Parent-File command is only for MF6 files. Use command Set-language-to-MF6, if applicable.",
+      );
+      return null;
+    }
+    const fileUri = document.uri;
     const fileName = path.basename(fileUri.fsPath);
     if (fileName === "mfsim.nam") {
       vscode.window.showInformationMessage(
@@ -15,23 +22,32 @@ export async function goToParent() {
     const filesInDir = await vscode.workspace.fs.readDirectory(dirUri);
     const config = vscode.workspace.getConfiguration("mf6Syntax");
     const maxFileSizeMB = config.get<number>("maxFileSizeMB", 50); // Default to 50MB
+    const excludedExtensions = [".grb", ".lst", ".hds", ".bud", ".csv"];
 
     for (const [name, type] of filesInDir) {
-      if (type === vscode.FileType.File && name !== fileName) {
-        const otherFileUri = vscode.Uri.joinPath(dirUri, name);
-        const stat = await vscode.workspace.fs.stat(otherFileUri);
-        if (stat.size > maxFileSizeMB * 1024 * 1024) {
-          continue;
-        }
-        const contentBytes = await vscode.workspace.fs.readFile(otherFileUri);
-        const content = Buffer.from(contentBytes).toString("utf-8");
+      if (
+        type !== vscode.FileType.File ||
+        name === fileName ||
+        excludedExtensions.some((ext) => name.endsWith(ext))
+      ) {
+        continue;
+      }
 
-        if (content.includes(fileName)) {
-          const parentDocument =
-            await vscode.workspace.openTextDocument(otherFileUri);
-          await vscode.window.showTextDocument(parentDocument);
-          return null;
-        }
+      const otherFileUri = vscode.Uri.joinPath(dirUri, name);
+      const stat = await vscode.workspace.fs.stat(otherFileUri);
+
+      if (stat.size > maxFileSizeMB * 1024 * 1024) {
+        continue;
+      }
+
+      const contentBytes = await vscode.workspace.fs.readFile(otherFileUri);
+      const content = Buffer.from(contentBytes).toString("utf-8");
+
+      if (content.includes(fileName)) {
+        const parentDocument =
+          await vscode.workspace.openTextDocument(otherFileUri);
+        await vscode.window.showTextDocument(parentDocument);
+        return null;
       }
     }
 
