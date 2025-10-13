2f055bd2cd1f17c35384e2349480c50d9acf97f8
martclanor <mart.dayniel.clanor@witteveenbos.com>
2025-10-13 20:30:54 +0200

    chore(mf6): update dfn files to MODFLOW 6.6.3 (#157)
    * Update dfn files (MODFLOW 6.6.3)
    
    * Parse prerelease field as bool
    
    * Ignore default field
    
    * Regenerate files from dfn's
    
    * Update grammar test based on new dfn's

--- utils/gen_from_dfn.py
+++ utils/gen_from_dfn.py
@@ -91,10 +91,12 @@ FIELD_PARSERS: dict[str, Callable[[Line], str | bool]] = {
     "netcdf": Line.parse_bool,
     "block_variable": Line.parse_bool,
     "just_data": Line.parse_bool,
+    "prerelease": Line.parse_bool,
 }
 
 IGNORED_FIELDS: frozenset[str] = frozenset(
     {
+        "default",
         "default_value",
         "deprecated",
         "extended",
@@ -132,6 +134,7 @@ class Section:
     netcdf: bool = False
     just_data: bool = False
     block_variable: bool = False
+    prerelease: bool = False
 
     @classmethod
     def from_dfn(cls, data: str) -> "Section":
